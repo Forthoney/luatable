15907789b882dfb363b66ea9353cd2359d0880d0
Seong-Heon Jung <castlehoneyjung@gmail.com>
2024-11-29 11:08:05 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# luatable
+A formal model of Lua 5.4's Table implementation
---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package "luatable" where
+  version := v!"0.1.0"
+
+lean_lib «Luatable» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe "luatable" where
+  root := `Main
